0087ca5ef86be6f1bfca7265d93869f39fe8fd08
Logan Brammer <logan.brammer@richardsonapparatus.com>
2022-12-05 22:43:02 +0000

    [ADD] Manifests and init

---
+++ space-mission/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Space Mission',
+    'summary': """Taking Odoo to the moon.""",
+    'description': """
+    Moon spaceboi
+    """,
+    'author': 'Logan',
+    'website': 'https://richardsonapparatus.com',
+    'category': 'Task Management',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [],
+    'demo': []
+}
