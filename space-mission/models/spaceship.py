8eb3c621613f09d2a000377f3decc05942139d08
RA-Logan <117311127+RA-Logan@users.noreply.github.com>
2022-12-09 13:59:32 -0600

    [ADD] API call

--- space-mission/models/spaceship.py
+++ space-mission/models/spaceship.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 class Spaceship(models.Model):
     _name = 'spacemission.spaceship'
@@ -12,6 +13,9 @@ class Spaceship(models.Model):
     dry_weight = fields.Float(string='Dry Weight')
     fuel_capacity = fields.Float(string='Fuel Capacity')
     crew_capacity = fields.Integer(string='Crew Capacity')
+    height = fields.Float(string="Height", default=0.0)
+    width = fields.Float(string="Width", default=0.0)
+    depth = fields.Float(string="Depth", default=0.0)
 
     build_status = fields.Selection(string='Build Status',
                                     selection=[('slated', 'Slated'),
@@ -21,3 +25,12 @@ class Spaceship(models.Model):
                                     copy=False)
 
     active = fields.Boolean(string='Active', default=True)
+
+    @api.constrains("height", "width")
+<<<<<<< Updated upstream
+    def _check_ship_dimens(self):
+        for record in self:
+            if record.width > record.height:
+                raise UserError('Width of ship cannot exceed height.')
+=======
+>>>>>>> Stashed changes
